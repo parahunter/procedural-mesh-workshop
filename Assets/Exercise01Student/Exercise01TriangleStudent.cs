98e96e49834440044744f82e2fcedd32a352165f
Alexander Birke <parahunter@gmail.com>
2019-04-02 14:44:25 +0100

    Copied over base and student folders

---
+++ Assets/Exercise01Student/Exercise01TriangleStudent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise01TriangleStudent : MonoBehaviour
+{
+	public Material mat;
+
+	void Start ()
+	{
+		MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+		MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+		renderer.material = mat;
+
+		Mesh mesh = new Mesh();
+		filter.mesh = mesh;
+
+		List<Vector3> vertices = new List<Vector3>();
+		vertices.Add(new Vector3(-1, -1, 0));
+		vertices.Add(new Vector3(0, 0.8f, 0));
+		vertices.Add(new Vector3(1, -1, 0));
+
+		List<int> triangles = new List<int>();
+		triangles.Add(0);
+		triangles.Add(1);
+		triangles.Add(2);
+		
+		mesh.SetVertices(vertices);
+		mesh.SetTriangles(triangles, 0);				
+	}
+}
---
+++ Assets/Exercise02Student/Exercise02TriangleStudent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise02TriangleStudent : MonoBehaviour
+{
+	public Material mat;
+
+	void Start()
+	{
+		MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+		MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+		renderer.material = mat;
+
+		Mesh mesh = new Mesh();
+		filter.mesh = mesh;
+
+		List<Vector3> vertices = new List<Vector3>();
+		vertices.Add(new Vector3(-1, -1, 0));
+		vertices.Add(new Vector3(0, 0.8f, 0));
+		vertices.Add(new Vector3(1, -1, 0));
+
+		List<Vector3> normals = new List<Vector3>();
+		normals.Add(Vector3.back);
+		normals.Add(Vector3.back);
+		normals.Add(Vector3.back);
+		
+		List<int> triangles = new List<int>();
+		triangles.Add(0);
+		triangles.Add(1);
+		triangles.Add(2);
+		
+		mesh.SetVertices(vertices);
+		mesh.SetNormals(normals);
+		mesh.SetTriangles(triangles, 0);
+	}
+}
